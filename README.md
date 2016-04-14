16698e298b206acea2f08f6489729f1b265a0d90
Joao Marcos <guilherme.malak@gmail.com>
2016-04-14 01:22:52 -0300

    README and adjustments

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-# execute #
-ruby csv_program.rb coupons.csv order_items.csv orders.csv products.csv output.csv
+# Sobre #
 
-ruby csv_program.rb coupons.csv order_items.csv orders.csv products.csv output_test.csv
+Programa para cálculo de pedidos levando em conta quantidade de itens e seus valores separados tal como aplicação de desconto com base em cupons.
+Feito em Ruby(testado na versão 2.3.0p0) para fins de teste para contratação pela empresa LUZ(luz.vc) para cargo de desenvolvedor Senior.
+
+# executando o programa #
+
+Para que o programa rode perfeitamente, se faz necessário chama-lo via linha de comando(ou terminal, como preferir..)
+passando os parâmetros certos para seu correto funcionamento. Todos os arquivos que devem ser passados já estão versionados.
+São eles:
+
+* coupons.csv: planilha com dados sobre os cupons de desconto
+* order_items.csv: planilha com dados dos itens referentes aos pedidos
+* order.csv: planilha com dados dos pedidos e seus cupons de desconto
+* products.csv: planilha com dados dos produtos e seus preços
+* output.csv: arquivo onde são gravados os resultados do programa, o id do pedido e seu preço final com os descontos aplicados
+
+Para o prorama rodar perfeitamente, basta chamar a linha abaixo:
+* ruby csv_program.rb coupons.csv order_items.csv orders.csv products.csv output.csv
+
+Os descontos são aplicados seguindo as regras propostas;
+* são dividos entre percentuais e absolutos
+* descontos absolutos dão um desconto bruto no valor final do pedido
+* descontos percentuais são calculados em 5% por item, a partir de dois e não ultrapassando 40%
+* um ticket de desconto não pode ser usado mais de 3x
+
+Ao rodar o programa e passar os dados corretamente, o programa será executado e seu resultado gravado no arquivo output.csv
+para conferência.
+
+# Observações finais #
+
+Vale ressaltar que, para fins de progressividade e continuidade nos testes do programa, os arquivos .csv não são alterados
+com exceção do output.csv, desta forma, o limite de uso dos cupons de desconto permanece inalterado ao término do programa
+permitindo assim sua execução posterior sem ser necessário restaurar o arquivo coupons.csv.
--- calculate.rb
+++ calculate.rb
@@ -8,6 +8,7 @@ def separateInArrays params
 	orders 		   = Array.new
 	orderItems   = Array.new
 	products     = Array.new
+	outputFile = nil
 
 	params.each do |param|
 		# fazer tratamento para cada tipo de arquivo diferente
@@ -16,7 +17,8 @@ def separateInArrays params
 		# pegar os preços no arquivo products de acordo com os itens do pedido(aqui temos os preços de cada produto)
 		# verificar os descontos no arquivo de descontos, devem ser usados os descontos na ordem que aparecem:
 		# OBS: Prestar atenção nos descontos vencidos e;ou já usados, o mesmo desconto só pode ser usado no max 3x. 
-		
+
+
 		case param
 			when 'coupons.csv'
 				verifyIfValidDiscount param, validCoupons
@@ -26,7 +28,8 @@ def separateInArrays params
 				pushToArray param, orders
 			when 'products.csv'
 				pushToArray param, products
-			when 'output_test.csv'
+			when 'output.csv'
+				outputFile = param
 				pushToArray param, orderItems
 			else
 				# stop program if invalid file is passed
@@ -35,13 +38,10 @@ def separateInArrays params
 		end
 	end
 	
-	mountOrders validCoupons, orders, orderItems, products
-		
-	# montar array com os valores finais dos pedidos e passar para writeOutput
-	# writeOutput finalValues
+	mountOrders validCoupons, orders, orderItems, products, outputFile
 end
 
-def mountOrders validCoupons, orders, orderItems, products
+def mountOrders validCoupons, orders, orderItems, products, outputFile
 	# pegar descontos
 	finalOrders = Array.new
 
@@ -67,14 +67,15 @@ def mountOrders validCoupons, orders, orderItems, products
 
 		# calculate discounts
 		validCoupons.each do |coupon|
-			# puts idOrder.to_s + '___1'
 			# verify if the OrderCoupon is in any coupon and coupon can be used'
 			if (coupon[0].to_i == idCoupon.to_i) and (coupon[4].to_i <= 3)
-				# puts idOrder.to_s + '___2'
 				if coupon[2] == 'percent'
-					# puts idOrder.to_s + '___3'
 					if totalCountItems > 1
 						discountInPercent = (totalCountItems * 5).to_f
+						# don't make discounts bigger than 40%
+						if discountInPercent > 40
+							discountInPercent = 40
+						end
 						totalValueOrder = (100 - discountInPercent).to_f * (totalValueOrder.to_f / 100)
 					end
 					totalValueOrder = totalValueOrder
@@ -87,7 +88,7 @@ def mountOrders validCoupons, orders, orderItems, products
 		end
 		finalOrders.push idOrder.to_s + ',' + totalValueOrder.to_s
 	end
-	writeOutput finalOrders
+	writeOutput finalOrders, outputFile
 end
 
 def pushToArray param, var
@@ -96,10 +97,14 @@ def pushToArray param, var
 	end
 end
 
-def writeOutput toWrite
-	File.open('output_test.csv', 'w') do |file| 
+def writeOutput toWrite, outputFile
+	File.open(outputFile, 'w') do |file|
 		toWrite.each do |line|
 			file.puts line
 		end
+
+		puts '=============================================================================='
+		puts 'Programa executado com Sucesso!!! dados gravados no arquivo "' + outputFile + '".'
+		puts '=============================================================================='
 	end
 end
